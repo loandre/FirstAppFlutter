1ffd0b389ab24ef93bd0a51e5862ef8462c55085
loandre <loandre.dev@gmail.com>
2023-10-03 03:26:19 -0300

    updated icons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,7 @@ const _backgroundGradientColors = [
   Color(0xFFd4dbda),
   Color(0xFF7990bc),
 ];
+const _backgroundGradientStops = [0.0, 0.8, 1.0];
 
 const _gradientColors = [
   Color(0xFF90A9DD),
@@ -15,8 +16,6 @@ const _gradientColors = [
 ];
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -27,27 +26,128 @@ class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
 
   @override
-  Widget build(BuildContext context) {
-    return ScaffoldMessenger(
-      key: _scaffoldMessengerKey,
-      child: Scaffold(
-        body: SafeArea(
-          child: Container(
-            decoration: _backgroundDecoration(),
-            child: Column(
-              children: [
-                _buildTopBar(),
-                SizedBox(height: 70),
-                _buildMiddleContent(),
-                _buildStatisticBoxes(),
-                Spacer(flex: 2),
-              ],
+Widget build(BuildContext context) {
+  return ScaffoldMessenger(
+    key: _scaffoldMessengerKey,
+     child: Scaffold(
+      backgroundColor: Colors.transparent,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: _backgroundDecoration(),
+                child: Column(
+                  children: [
+                    _buildTopBar(),
+                    SizedBox(height: 70),
+                    _buildMiddleContent(),
+                    _buildStatisticBoxes(),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
             ),
-          ),
+            _buildPortfolio(),
+          ],
         ),
-        bottomNavigationBar: _buildBottomNavigationBar(),
+      ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    ),
+  );
+}
+
+
+  Widget _buildPortfolio() {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      padding: EdgeInsets.all(_defaultPaddingValue),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(14),
+          topRight: Radius.circular(14),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Portfolio',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              Icon(Icons.more_vert, size: 28),
+            ],
+          ),
+          SizedBox(height: 15),
+          ..._buildPortfolioItems(),
+        ],
       ),
     );
+  }
+
+  List<Widget> _buildPortfolioItems() {
+    final iconPaths = [
+      'lib/icons/ltc-litecoin.png',
+      'lib/icons/bnb-binancecoin.png',
+      'lib/icons/eth-ethereum.png',
+      'lib/icons/link-chainlink.png'
+    ];
+    final titles = ['LTC', 'BNB', 'ETH', 'LINK'];
+    final subtitles = ['Litecoin', 'Binance Coin', 'Ethereum', 'Chainlink'];
+    final values = ['23.21', '45.12', '1,200.43', '30.05'];
+    final details = [
+      '1,571.5 USD (+1.9%)',
+      '1,114.29 USD (+4.8%)',
+      '17,437.56 USD (+2.1%)',
+      '493.21 USD (+0.3%)'
+    ];
+
+    List<Widget> portfolioItems = [];
+    for (int i = 0; i < titles.length; i++) {
+      portfolioItems.add(
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          child: Row(
+            children: [
+              Image.asset(iconPaths[i], width: 40, height: 40),
+              SizedBox(width: 12.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(titles[i],
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18)),
+                    SizedBox(height: 2.0),
+                    Text(subtitles[i],
+                        style: TextStyle(color: Colors.grey, fontSize: 14)),
+                  ],
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(values[i],
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                  SizedBox(height: 2.0),
+                  Text(details[i],
+                      style: TextStyle(color: Colors.grey[400], fontSize: 14)),
+                ],
+              ),
+              SizedBox(width: 8.0),
+            ],
+          ),
+        ),
+      );
+      if (i != titles.length - 1) {
+        portfolioItems.add(Divider(color: Colors.grey[300]));
+      }
+    }
+    return portfolioItems;
   }
 
   BottomNavigationBar _buildBottomNavigationBar() {
@@ -103,7 +203,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildTopBar() {
     return Padding(
-      padding: _defaultPadding(),
+      padding: EdgeInsets.fromLTRB(
+        _defaultPaddingValue,
+        55.0,
+        _defaultPaddingValue,
+        0.0,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -156,7 +261,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Stack(
       alignment: Alignment.topRight,
       children: [
-        Icon(Icons.notifications, size: 24),
+        Icon(Icons.notifications, size: 24, color: Colors.white),
         CircleAvatar(radius: 6, backgroundColor: Colors.orange),
       ],
     );
@@ -166,22 +271,23 @@ class _MyHomePageState extends State<MyHomePage> {
     return Column(
       children: [
         Text('\$ 21,937.31',
-            style: _middleAmountTextStyle().copyWith(fontSize: 29)),
+            style: TextStyle(fontSize: 29, fontWeight: FontWeight.bold)),
         SizedBox(height: 10),
         Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.show_chart, color: Colors.green, size: 20),
-            Text('APY + 7.21', style: TextStyle(fontSize: 14)),
-          ],
-        ),
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    Image.asset('lib/icons/show_chart.png', width: 20, height: 20, color: Colors.green),
+    SizedBox(width: 5),
+    Text('APY + 7.21', style: TextStyle(fontSize: 14)),
+  ],
+),
       ],
     );
   }
 
   Widget _buildStatisticBoxes() {
     return Padding(
-      padding: _defaultPadding(),
+      padding: EdgeInsets.all(_defaultPaddingValue),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
@@ -198,7 +304,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildStatisticBox(String value, String timeFrame) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 8.0),
-      padding: _defaultPadding(),
+      padding: EdgeInsets.all(_defaultPaddingValue),
       decoration: _statisticBoxDecoration(),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -211,28 +317,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  BoxDecoration _backgroundDecoration() => BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: _backgroundGradientColors,
-        ),
-      );
+  BoxDecoration _backgroundDecoration() {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        colors: _backgroundGradientColors,
+      ),
+    );
+  }
 
-  EdgeInsets _defaultPadding() => EdgeInsets.all(_defaultPaddingValue);
+  BoxDecoration _copyContainerDecoration() {
+    return BoxDecoration(
+      borderRadius: BorderRadius.circular(12),
+      color: Colors.black,
+    );
+  }
 
-  BoxDecoration _copyContainerDecoration() => BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.black,
-      );
-
-  TextStyle _middleAmountTextStyle() => TextStyle(
-        fontSize: 24,
-        fontWeight: FontWeight.bold,
-      );
-
-  BoxDecoration _statisticBoxDecoration() => BoxDecoration(
-        color: Colors.grey.shade600.withOpacity(0.6),
-        borderRadius: BorderRadius.circular(12),
-      );
+  BoxDecoration _statisticBoxDecoration() {
+    return BoxDecoration(
+      color: Colors.grey.shade800.withOpacity(0.5),
+      borderRadius: BorderRadius.circular(12),
+    );
+  }
 }
